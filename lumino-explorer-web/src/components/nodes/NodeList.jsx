54ccf20b8b09931ec9e94ec196ed41b40037542f
Gaspar Medina <gmedina@infuy.com>
2019-06-13 17:30:58 -0300

    [FIX] version endpoint and bug
    Control when node not have channels

--- lumino-explorer-web/src/components/nodes/NodeList.jsx
+++ lumino-explorer-web/src/components/nodes/NodeList.jsx
@@ -4,7 +4,11 @@ import NodeItem from "./NodeItem";
 export default class NodeList extends Component {
     render =()=> {
         let nodesItemList = this.props.nodeList.map((e)=>{
-            return <NodeItem node_address={e.node_address} rns_address={e.rns_address} openChannelsCount={e.channels.length}/>
+            let channelLenght = 0;
+            if (e.channels){
+                channelLenght = e.channels.length;
+            }
+            return <NodeItem node_address={e.node_address} rns_address={e.rns_address} openChannelsCount={channelLenght}/>
         });
 
         return <ul className="list-unstyled row channel-list">
