2b6f7914f5977c1441433fff9de9335e002920fa
Gaspar Medina <gmedina@infuy.com>
2019-06-19 18:00:45 -0300

    [ADD] balance channel
    Channel balance is added in channel details page. Now when click in graph edge it shows a channel summary. Also when a new channel deposit is read a new feed is show with deposit details.

--- lumino-explorer-web/src/containers/dashboard/DashboardContainer.jsx
+++ lumino-explorer-web/src/containers/dashboard/DashboardContainer.jsx
@@ -7,6 +7,7 @@ import TokenSelect from "../../components/tokenSelect/TokenSelect";
 import SummaryInfo from "../summaryInfo/SummaryInfo";
 import FeedContainer from "../feedContainer/FeedContainer";
 import Graph from "react-graph-vis";
+import {fromWei} from "../../lib/amounts/weiConversion";
 
 const graphOptions = {
     layout: {
@@ -35,13 +36,15 @@ const graphOptions = {
         },
     },
     edges: {
+        title: "test",
         color: "#000000",
         width: 2,
+        label: "pepe",
         arrows: {
             to:     {enabled: false, scaleFactor:1, type:'arrow'},
             middle: {enabled: false, scaleFactor:1, type:'arrow'},
             from:   {enabled: false, scaleFactor:1, type:'arrow'}
-            },
+            }
     },
 
     height: "400px"
@@ -52,26 +55,71 @@ class DashboardContainer extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            isSummaryActive: false,
+            isSummaryNodeActive: false,
+            isSummaryChannelActive : false,
             addressFrom: null,
             addressTo: null,
         };
-
-        this.hideNodeSummary = this.hideNodeSummary.bind(this);
-        this.showNodeSummary = this.showNodeSummary.bind(this);
     }
 
-    hideNodeSummary() {
+    hideSummaries() {
         this.setState({
-            isSummaryActive: false,
+            isSummaryNodeActive: false,
+            isSummaryChannelActive: false,
+            summaryNodeList: null,
+            summaryChannelList: null
         });
     }
 
-    showNodeSummary(summaryNodeList) {
-        this.setState({
-            isSummaryActive: true,
-            summaryNodeList: summaryNodeList,
-        });
+    showNodeSummary(nodes) {
+        if(nodes && (nodes.length === 1)) {
+            // we have one node selected
+            // we get the index of the selected node
+            let nodeIndex = nodes[0];
+            let fullNodeList = this.props.dashboardData.nodes;
+            let node = fullNodeList[nodeIndex];
+
+            let widget = <div className="fs-14">
+                <p className="text-green m-0 w-600">Node Summary</p>
+                <p className="m-0"><span className="text-green d-inline-block d-sm-inline">RNS Address: </span> <span className="d-inline-block d-sm-inline">{node.rns_address}</span></p>
+                <p className="m-0"><span className="text-green d-inline-block d-sm-inline">RSK Address: </span> <span className="summary-rsk-address d-inline-block d-sm-inline w-99">{node.node_address}</span></p>
+            </div>
+
+            this.setState({
+                isSummaryNodeActive: true,
+                isSummaryChannelActive: false,
+                summaryNodeList: widget,
+            });
+        }
+    }
+
+    showChannelSummary(edges,nodes,  nodeGraph) {
+        if (edges.length > 0 && (nodes.length === 0)) {
+            let widget;
+            edges.forEach((channelIdentifier) => {
+                nodeGraph.edges.forEach((edgeGraph) => {
+                    if (channelIdentifier === edgeGraph.id) {
+                        widget = <div className="fs-14">
+                            <p className="text-green m-0 w-600">Channel Summary</p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">ID: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.id}</span></p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">Participant 1: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.from_rns_address} ({edgeGraph.from_address})</span></p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">Participant 2: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.to_rns_address} ({edgeGraph.to_address})</span></p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">Token: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.token_name} ({edgeGraph.token_address})</span></p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">Balance: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.balance} </span></p>
+                            <p className="m-0"><span className="text-green d-inline-block d-sm-inline">Status: </span> <span className="d-inline-block d-sm-inline">{edgeGraph.state}</span></p>
+                        </div>
+                    }
+                });
+
+            });
+
+            this.setState({
+                isSummaryChannelActive: true,
+                isSummaryNodeActive: false,
+                summaryChannelList: widget,
+            });
+        }
+
     }
 
     render=()=>{
@@ -96,7 +144,7 @@ class DashboardContainer extends Component{
             tokenSelect = <TokenSelect tokenList={this.props.dashboardData.tokens}/>;
         }
 
-    
+
         let theNodes = [];
         let theEdges = [];
         if(this.props.dashboardData) {
@@ -111,7 +159,16 @@ class DashboardContainer extends Component{
                     if (source && target) {
                         return {
                             from: source.id,
-                            to: target.id
+                            to: target.id,
+                            balance: fromWei(channel.total_deposit, channel.token_decimals),
+                            from_rns_address : channel.from_rns_address,
+                            to_rns_address : channel.to_rns_address,
+                            from_address : channel.from_address,
+                            to_address : channel.to_address,
+                            state : channel.state,
+                            token_name : channel.token_name,
+                            token_address : channel.token_address,
+                            id: channel.channel_identifier,
                         }
                     }else{
                         return null;
@@ -138,21 +195,9 @@ class DashboardContainer extends Component{
                             options={graphOptions}
                             events={{
                                 select: (event) => {
-                                    let { nodes } = event;
-                                    if(nodes && (nodes.length === 1)) {
-                                        // we have one node selected
-                                        // we get the index of the selected node
-                                        let nodeIndex = nodes[0];
-                                        let fullNodeList = this.props.dashboardData.nodes;
-                                        let node = fullNodeList[nodeIndex];
-
-                                        let widget = <div className="fs-14">
-                                        <p className="text-green m-0 w-600">Node Summary</p>
-                                        <p className="m-0"><span className="text-green d-inline-block d-sm-inline">RNS Address: </span> <span className="d-inline-block d-sm-inline">{node.rns_address}</span></p>
-                                        <p className="m-0"><span className="text-green d-inline-block d-sm-inline">RSK Address: </span> <span className="summary-rsk-address d-inline-block d-sm-inline w-99">{node.node_address}</span></p>
-                                        </div>
-                                        this.showNodeSummary(widget);
-                                    }
+                                    var { nodes, edges } = event;
+                                    this.showNodeSummary(nodes);
+                                    this.showChannelSummary(edges, nodes,nodeGraph);
                                 }
                             }}
                             getNetwork={network => {
@@ -161,14 +206,24 @@ class DashboardContainer extends Component{
                         />
                     </div>
 
-                    {this.state.isSummaryActive && <div className="w-100 summary-node fadeInDown animated mb-2 px-3 px-md-0">
+                    {this.state.isSummaryNodeActive && <div className="w-100 summary-node fadeInDown animated mb-2 px-3 px-md-0">
                         <div className="bg-white rounded p-3 position-relative">
                             {this.state.summaryNodeList}
-                            <button type="button" className="close summary-node__close position-absolute" data-dismiss="alert" aria-label="Close" onClick={() => this.hideNodeSummary()}>
+                            <button type="button" className="close summary-node__close position-absolute" data-dismiss="alert" aria-label="Close" onClick={() => this.hideSummaries()}>
                                 <i className="fal fa-times"> </i>
                             </button>
                         </div>
                     </div>}
+
+                    {this.state.isSummaryChannelActive && <div className="w-100 summary-node fadeInDown animated mb-2 px-3 px-md-0">
+                        <div className="bg-white rounded p-3 position-relative">
+                            {this.state.summaryChannelList}
+                            <button type="button" className="close summary-node__close position-absolute" data-dismiss="alert" aria-label="Close" onClick={() => this.hideSummaries()}>
+                                <i className="fal fa-times"> </i>
+                            </button>
+                        </div>
+                    </div>}
+
                 </div>
                 <FeedContainer/>
             </div>
